852ac2dda5a6b75b951d23f2329e8e04792f863c
Hae <lhauraineangeliclee@gmail.com>
2026-09-15 09:09:58 +0900

    Fix ADMINS declaration and formatting

--- Money.py
+++ Money.py
@@ -1,9 +1,8 @@
 # 👑 Cleydo Admins
-ADMINS = {
-    "https://www.facebook.com/share/1Zb1n5SYo6/"
-}def is_admin(user_id):
-    return str(user_id) in ADMINS
+ADMINS = {"https://www.facebook.com/share/1Zb1n5SYo6/"}
 
+def is_admin(user_id):
+    return str(user_id) in ADMINS
 
 def admin_add_money(admin_id, target_id, amount):
     if not is_admin(admin_id):
